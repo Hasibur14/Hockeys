3e58751cf1ea1eb8fd2376b5b7659faccd933385
hasibur14 <hasib141415@gmail.com>
2024-01-24 12:41:02 +0600

    update banner  section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage:{
+          hero: "url('./images/banne-1.png')",
+        }
+      },
     },
     plugins: [],
   }
